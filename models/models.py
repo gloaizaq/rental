d28f0e0c0be61b0a3fc8a1d9b4954c8f5feb4f62
Gustavo Loaiza <loaizagustavo95@gmail.com>
2018-06-26 19:43:38 -0600

    floor model created and its relation with shop model

--- models/models.py
+++ models/models.py
@@ -9,3 +9,10 @@ class Shop(models.Model):
     area = fields.Float(string="Area")
     watermeter = fields.Integer(string="Medidor Agua")
     currentmeter = fields.Integer(string="Medidor Electrico")
+    floor_id = fields.Many2one('rental.floor', string="Piso")
+
+class Floor(models.Model):
+    _name = 'rental.floor'
+
+    number = fields.Integer(string="Numero")
+    shop = fields.One2many('rental.shop', 'floor_id', string="Locales")
